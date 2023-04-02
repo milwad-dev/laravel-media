fd7e8e6c35fded3e2c3e6569323ae2c361b3dd1e
miladev-ent <milwad.dev@gmail.com>
2023-04-02 20:39:43 +0330

    add `require` for composer and create LaravelMediaServiceProvider

---
+++ src/LaravelMediaServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Milwad\LaravelMedia;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMediaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+
+    }
+}
