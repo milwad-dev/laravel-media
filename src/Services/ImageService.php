73ec51ffff89b00f8344eb92767760df163316f1
miladev-ent <milwad.dev@gmail.com>
2023-05-09 20:11:26 +0330

    complete `upload` function in ImageService

--- src/Services/ImageService.php
+++ src/Services/ImageService.php
@@ -3,11 +3,30 @@
 namespace Milwad\LaravelMedia\Services;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Milwad\LaravelMedia\Enums\MediaTypeEnum;
+use Milwad\LaravelMedia\Models\Media;
 
 class ImageService
 {
-    public function upload(UploadedFile $file, string $filename, string $dir)
+    public function upload(UploadedFile $file, bool $isPrivate = false)
     {
-        // TODO
+        $filename = $file->getClientOriginalName();
+
+        // Upload file
+        Storage::putFileAs($path = 'images', $file, $filename.'-'.now());
+
+        // Create a row for migration
+        Media::query()->create([
+            'user_id' => auth()->id(),
+            'filename' => $filename,
+            'path' => $path,
+            'is_private' => $isPrivate,
+            'files' => [],
+            'type' => MediaTypeEnum::TYPE_IMAGE->value,
+        ]);
+
+        // Return filename
+        return $filename;
     }
 }
