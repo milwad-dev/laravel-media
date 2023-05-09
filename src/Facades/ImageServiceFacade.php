b2c34c0b826b677d362bf0df643aabf77981312e
miladev-ent <milwad.dev@gmail.com>
2023-05-09 20:34:44 +0330

    add ImageService methods to related Facade

--- src/Facades/ImageServiceFacade.php
+++ src/Facades/ImageServiceFacade.php
@@ -2,8 +2,12 @@
 
 namespace Milwad\LaravelMedia\Facades;
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static upload(UploadedFile $file, bool $isPrivate = false)
+ */
 class ImageServiceFacade extends Facade
 {
     protected static function getFacadeAccessor()
